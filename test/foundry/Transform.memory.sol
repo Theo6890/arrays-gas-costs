e0cac09c57504b41d30c6160ed947b21a06e0064
Théo RICHARD <mail@rcode.tech>
2023-03-27 10:32:25 +0200

    refactor(test): add in test names `result_in_`

--- test/foundry/Transform.memory.sol
+++ test/foundry/Transform.memory.sol
@@ -15,62 +15,88 @@ contract TransformTest is Test {
     /*//////////////////////////////////////////////////////////////
                             SOLIDITY
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_50_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_50_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(50_000);
     }
 
-    function test_transformRandomsToMatchRange_30_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_30_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(30_000);
     }
 
-    function test_transformRandomsToMatchRange_20_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_20_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(20_000);
     }
 
-    function test_transformRandomsToMatchRange_10_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_10_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(10_000);
     }
 
-    function test_transformRandomsToMatchRange_5_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_5_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(5_000);
     }
 
-    function test_transformRandomsToMatchRange_1_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_1_000()
+        public
+    {
         instance.transformRandoms_result_to_memory(1_000);
     }
 
-    function test_transformRandomsToMatchRange_500() public {
+    function test_transformRandomsToMatchRange_result_to_memory_500() public {
         instance.transformRandoms_result_to_memory(500);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  YUL
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_Yul_50_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_50_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(50_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_30_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_30_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(30_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_20_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_20_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(20_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_10_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_10_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(10_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_5_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_5_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(5_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_1_000() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_1_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(1_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_500() public {
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_500()
+        public
+    {
         instance.YUL_transformRandoms_result_to_memory(500);
     }
 }
--- test/foundry/Transform.storage.sol
+++ test/foundry/Transform.storage.sol
@@ -15,62 +15,88 @@ contract TransformTest is Test {
     /*//////////////////////////////////////////////////////////////
                                  SOLIDITY
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_50_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_50_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(50_000);
     }
 
-    function test_transformRandomsToMatchRange_30_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_30_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(30_000);
     }
 
-    function test_transformRandomsToMatchRange_20_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_20_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(20_000);
     }
 
-    function test_transformRandomsToMatchRange_10_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_10_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(10_000);
     }
 
-    function test_transformRandomsToMatchRange_5_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_5_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(5_000);
     }
 
-    function test_transformRandomsToMatchRange_1_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_1_000()
+        public
+    {
         instance.transformRandoms_result_to_storage(1_000);
     }
 
-    function test_transformRandomsToMatchRange_500() public {
+    function test_transformRandomsToMatchRange_result_to_storage_500() public {
         instance.transformRandoms_result_to_storage(500);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  YUL
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_Yul_50_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_50_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(50_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_30_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_30_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(30_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_20_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_20_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(20_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_10_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_10_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(10_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_5_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_5_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(5_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_1_000() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_1_000()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(1_000);
     }
 
-    function test_transformRandomsToMatchRange_Yul_500() public {
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_500()
+        public
+    {
         instance.YUL_transformRandoms_result_to_storage(500);
     }
 }
