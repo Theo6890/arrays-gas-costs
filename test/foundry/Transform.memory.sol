6e1fcdb612845a43472c1e6fb147333777ef4e90
Théo RICHARD <mail@rcode.tech>
2023-03-28 11:10:20 +0200

    test: update tests for realistic scenarios with set randoms
    Estimate gas costs more precisely by setting manually the received randoms

--- test/foundry/Transform.memory.sol
+++ test/foundry/Transform.memory.sol
@@ -8,107 +8,206 @@ import "../../src/Transform.sol";
 contract TransformTest is Test {
     Transform public instance;
 
+    uint256[] randoms_27_000;
+    uint256[] randoms_22_000;
+    uint256[] randoms_19_000;
+    uint256[] randoms_11_000;
+    uint256[] randoms_5_000;
+    uint256[] randoms_2_300;
+    uint256[] randoms_834;
+    uint256[] randoms_124;
+    uint256[] randoms_75;
+
     function setUp() public {
         instance = new Transform();
+
+        for (uint256 i; i < 27_000; ++i) {
+            randoms_27_000.push(12489031409234 + i);
+            if (i < 22_000) randoms_22_000.push(12489031409234 + i);
+            if (i < 19_000) randoms_19_000.push(12489031409234 + i);
+            if (i < 11_000) randoms_11_000.push(12489031409234 + i);
+            if (i < 5_000) randoms_5_000.push(12489031409234 + i);
+            if (i < 2_300) randoms_2_300.push(12489031409234 + i);
+            if (i < 834) randoms_834.push(12489031409234 + i);
+            if (i < 124) randoms_124.push(12489031409234 + i);
+            if (i < 75) randoms_75.push(12489031409234 + i);
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
                             SOLIDITY
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_result_to_memory_50_000()
+    function test_transformRandomsToMatchRange_result_to_memory_27_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(50_000);
+        instance.setRandoms(randoms_27_000);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            27_000
+        );
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_45_000()
+    function test_transformRandomsToMatchRange_result_to_memory_22_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(45_000);
+        instance.setRandoms(randoms_22_000);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            22_000
+        );
+        assertEq(result.length, 22_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_30_000()
+    function test_transformRandomsToMatchRange_result_to_memory_19_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(30_000);
+        instance.setRandoms(randoms_19_000);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            19_000
+        );
+        assertEq(result.length, 19_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_20_000()
+    function test_transformRandomsToMatchRange_result_to_memory_11_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(20_000);
+        instance.setRandoms(randoms_11_000);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            11_000
+        );
+        assertEq(result.length, 11_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_10_000()
+    function test_transformRandomsToMatchRange_result_to_memory_5_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(10_000);
+        instance.setRandoms(randoms_5_000);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            5_000
+        );
+        assertEq(result.length, 5_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_5_000()
+    function test_transformRandomsToMatchRange_result_to_memory_2_300_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(5_000);
+        instance.setRandoms(randoms_2_300);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            2_300
+        );
+        assertEq(result.length, 2_300);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_1_000()
+    function test_transformRandomsToMatchRange_result_to_memory_834_randoms()
         public
     {
-        instance.transformRandoms_result_to_memory(1_000);
+        instance.setRandoms(randoms_834);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            834
+        );
+        assertEq(result.length, 834);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_500() public {
-        instance.transformRandoms_result_to_memory(500);
+    function test_transformRandomsToMatchRange_result_to_memory_124_randoms()
+        public
+    {
+        instance.setRandoms(randoms_124);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            124
+        );
+        assertEq(result.length, 124);
+    }
+
+    function test_transformRandomsToMatchRange_result_to_memory_75_randoms()
+        public
+    {
+        instance.setRandoms(randoms_75);
+        uint256[] memory result = instance.transformRandoms_result_to_memory(
+            75
+        );
+        assertEq(result.length, 75);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  YUL
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_50_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_27_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(50_000);
+        instance.setRandoms(randoms_27_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(27_000);
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_45_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_22_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(45_000);
+        instance.setRandoms(randoms_22_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(27_000);
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_30_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_19_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(30_000);
+        instance.setRandoms(randoms_19_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(19_000);
+        assertEq(result.length, 19_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_20_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_11_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(20_000);
+        instance.setRandoms(randoms_11_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(11_000);
+        assertEq(result.length, 11_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_10_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_5_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(10_000);
+        instance.setRandoms(randoms_5_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(5_000);
+        assertEq(result.length, 5_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_5_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_2_300_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(5_000);
+        instance.setRandoms(randoms_2_300);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(2_300);
+        assertEq(result.length, 2_300);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_1_000()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_834_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(1_000);
+        instance.setRandoms(randoms_834);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(834);
+        assertEq(result.length, 834);
     }
 
-    function test_transformRandomsToMatchRange_result_to_memory_Yul_500()
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_124_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_memory(500);
+        instance.setRandoms(randoms_124);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(124);
+        assertEq(result.length, 124);
+    }
+
+    function test_transformRandomsToMatchRange_result_to_memory_Yul_75_randoms()
+        public
+    {
+        instance.setRandoms(randoms_75);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_memory(75);
+        assertEq(result.length, 75);
     }
 }
--- test/foundry/Transform.storage.sol
+++ test/foundry/Transform.storage.sol
@@ -8,107 +8,206 @@ import "../../src/Transform.sol";
 contract TransformTest is Test {
     Transform public instance;
 
+    uint256[] randoms_27_000;
+    uint256[] randoms_22_000;
+    uint256[] randoms_19_000;
+    uint256[] randoms_11_000;
+    uint256[] randoms_5_000;
+    uint256[] randoms_2_300;
+    uint256[] randoms_834;
+    uint256[] randoms_124;
+    uint256[] randoms_75;
+
     function setUp() public {
         instance = new Transform();
+
+        for (uint256 i; i < 27_000; ++i) {
+            randoms_27_000.push(12489031409234 + i);
+            if (i < 22_000) randoms_22_000.push(12489031409234 + i);
+            if (i < 19_000) randoms_19_000.push(12489031409234 + i);
+            if (i < 11_000) randoms_11_000.push(12489031409234 + i);
+            if (i < 5_000) randoms_5_000.push(12489031409234 + i);
+            if (i < 2_300) randoms_2_300.push(12489031409234 + i);
+            if (i < 834) randoms_834.push(12489031409234 + i);
+            if (i < 124) randoms_124.push(12489031409234 + i);
+            if (i < 75) randoms_75.push(12489031409234 + i);
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
-                                 SOLIDITY
+                            SOLIDITY
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_result_to_storage_50_000()
+    function test_transformRandomsToMatchRange_result_to_storage_27_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(50_000);
+        instance.setRandoms(randoms_27_000);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            27_000
+        );
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_45_000()
+    function test_transformRandomsToMatchRange_result_to_storage_22_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(45_000);
+        instance.setRandoms(randoms_22_000);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            22_000
+        );
+        assertEq(result.length, 22_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_30_000()
+    function test_transformRandomsToMatchRange_result_to_storage_19_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(30_000);
+        instance.setRandoms(randoms_19_000);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            19_000
+        );
+        assertEq(result.length, 19_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_20_000()
+    function test_transformRandomsToMatchRange_result_to_storage_11_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(20_000);
+        instance.setRandoms(randoms_11_000);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            11_000
+        );
+        assertEq(result.length, 11_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_10_000()
+    function test_transformRandomsToMatchRange_result_to_storage_5_000_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(10_000);
+        instance.setRandoms(randoms_5_000);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            5_000
+        );
+        assertEq(result.length, 5_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_5_000()
+    function test_transformRandomsToMatchRange_result_to_storage_2_300_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(5_000);
+        instance.setRandoms(randoms_2_300);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            2_300
+        );
+        assertEq(result.length, 2_300);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_1_000()
+    function test_transformRandomsToMatchRange_result_to_storage_834_randoms()
         public
     {
-        instance.transformRandoms_result_to_storage(1_000);
+        instance.setRandoms(randoms_834);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            834
+        );
+        assertEq(result.length, 834);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_500() public {
-        instance.transformRandoms_result_to_storage(500);
+    function test_transformRandomsToMatchRange_result_to_storage_124_randoms()
+        public
+    {
+        instance.setRandoms(randoms_124);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            124
+        );
+        assertEq(result.length, 124);
+    }
+
+    function test_transformRandomsToMatchRange_result_to_storage_75_randoms()
+        public
+    {
+        instance.setRandoms(randoms_75);
+        uint256[] memory result = instance.transformRandoms_result_to_storage(
+            75
+        );
+        assertEq(result.length, 75);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  YUL
     //////////////////////////////////////////////////////////////*/
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_50_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_27_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(50_000);
+        instance.setRandoms(randoms_27_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(27_000);
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_45_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_22_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(45_000);
+        instance.setRandoms(randoms_22_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(27_000);
+        assertEq(result.length, 27_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_30_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_19_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(30_000);
+        instance.setRandoms(randoms_19_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(19_000);
+        assertEq(result.length, 19_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_20_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_11_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(20_000);
+        instance.setRandoms(randoms_11_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(11_000);
+        assertEq(result.length, 11_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_10_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_5_000_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(10_000);
+        instance.setRandoms(randoms_5_000);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(5_000);
+        assertEq(result.length, 5_000);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_5_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_2_300_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(5_000);
+        instance.setRandoms(randoms_2_300);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(2_300);
+        assertEq(result.length, 2_300);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_1_000()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_834_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(1_000);
+        instance.setRandoms(randoms_834);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(834);
+        assertEq(result.length, 834);
     }
 
-    function test_transformRandomsToMatchRange_result_to_storage_Yul_500()
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_124_randoms()
         public
     {
-        instance.YUL_transformRandoms_result_to_storage(500);
+        instance.setRandoms(randoms_124);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(124);
+        assertEq(result.length, 124);
+    }
+
+    function test_transformRandomsToMatchRange_result_to_storage_Yul_75_randoms()
+        public
+    {
+        instance.setRandoms(randoms_75);
+        uint256[] memory result = instance
+            .YUL_transformRandoms_result_to_storage(75);
+        assertEq(result.length, 75);
     }
 }
