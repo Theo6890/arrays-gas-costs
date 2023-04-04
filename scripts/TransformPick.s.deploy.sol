f8257fcfb5e097e05f80bf00e42b3b03282c3ae2
Théo RICHARD <mail@rcode.tech>
2023-04-04 17:43:56 +0200

    docs: update verify comment

--- scripts/TransformPick.s.deploy.sol
+++ scripts/TransformPick.s.deploy.sol
@@ -5,12 +5,13 @@ import "forge-std/Script.sol";
 import {TransformPick} from "../src/TransformPick.sol";
 
 /**
-* @dev forge script scripts/TransformPick.s.deploy.sol:TransformPick_deploy --rpc-url $MUMBAI_RPC --broadcast --verify -vvvv
+* @dev forge script scripts/TransformPick.s.deploy.sol:TransformPick_deploy --rpc-url $MUMBAI_RPC --broadcast --verify -vvvv --optimise --optimizer-runs 200 -w
 *
 * @dev If verification fails:
 * forge verify-contract \
     --chain 80001 \
-    --num-of-optimizations 200 \
+    --num-of-optimizations 20000 \
+    --compiler-version v0.8.13+commit.abaa5c0e \
     --watch 0x8994007ba2e02248225997454e734430265e5045 src/TransformPick.sol:TransformPick $POLYGON_KEY
 */
 contract TransformPick_deploy is Script {
