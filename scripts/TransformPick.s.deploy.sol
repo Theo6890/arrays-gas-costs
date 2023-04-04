8bd88e5bb67d69b9cc4dcdd073bf6544848bb72e
Théo RICHARD <mail@rcode.tech>
2023-04-04 17:35:52 +0200

    chore: deploy and produce fake randoms solidity scripts

---
+++ scripts/TransformPick.s.deploy.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {TransformPick} from "../src/TransformPick.sol";
+
+/**
+* @dev forge script scripts/TransformPick.s.deploy.sol:TransformPick_deploy --rpc-url $MUMBAI_RPC --broadcast --verify -vvvv
+*
+* @dev If verification fails:
+* forge verify-contract \
+    --chain 80001 \
+    --num-of-optimizations 200 \
+    --watch 0x8994007ba2e02248225997454e734430265e5045 src/TransformPick.sol:TransformPick $POLYGON_KEY
+*/
+contract TransformPick_deploy is Script {
+    function run() external {
+        ///@dev Configure .env file
+        string memory SEED = vm.envString("SEED");
+        uint256 privateKey = vm.deriveKey(SEED, 0); // address at index 0
+        vm.startBroadcast(privateKey);
+
+        TransformPick transform = new TransformPick();
+
+        vm.stopBroadcast();
+    }
+}
---
+++ scripts/TransformPick.s.produceFakeRandoms.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import {TransformPick} from "../src/TransformPick.sol";
+
+// forge script scripts/TransformPick.s.produceFakeRandoms.sol:TransformPick_produceFakeRandoms --rpc-url $MUMBAI_RPC --broadcast -vvvv
+contract TransformPick_produceFakeRandoms is Script {
+    function run() external {
+        ///@dev automatic load from .env file
+        string memory SEED = vm.envString("SEED");
+        uint256 privateKey = vm.deriveKey(SEED, 0); // address at index 0
+        vm.startBroadcast(privateKey);
+
+        TransformPick transform = TransformPick(
+            0xBffEaf52DB22A9D73fd2882975b446e8fE8f60dc
+        );
+
+        transform.produceFakeRandoms(0, 10_000);
+
+        vm.stopBroadcast();
+    }
+}
